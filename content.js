279c9c4dc31b9a50a963d39ee2513fba69421a96
Ian Rackow <ian.rackow@gmail.com>
2023-01-28 11:05:02 -0500

    Update content.js

--- content.js
+++ content.js
@@ -1,12 +1,11 @@
-
 var j = document.createElement('script');
-j.src = chrome.extension.getURL('jquery-1.10.2.min.js');
+j.src = chrome.runtime.getURL('jquery-1.10.2.min.js');
 (document.head || document.documentElement).appendChild(j);
 
 var g = document.createElement('script');
-g.src = chrome.extension.getURL('gmail.js');
+g.src = chrome.runtime.getURL('gmail.js');
 (document.head || document.documentElement).appendChild(g);
 
 var s = document.createElement('script');
-s.src = chrome.extension.getURL('main.js');
+s.src = chrome.runtime.getURL('main.js');
 (document.head || document.documentElement).appendChild(s);
